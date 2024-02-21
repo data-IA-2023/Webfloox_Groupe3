283e031ad8c84e8aeac58b896846fa42cddf1529
NaoufelGreta <naoufelboutet2@gmail.com>
2024-02-21 09:50:05 +0100

    'test'

--- Codes/ApplicationAPI.py
+++ Codes/ApplicationAPI.py
@@ -5,9 +5,10 @@ Created on Tue Feb 20 10:16:34 2024
 @author: naouf
 """
 
-from fastapi import FastAPI, Request
+from fastapi  import FastAPI, Request
 app = FastAPI()
 @app.get("/")
 def get_root(request:Request):
     return{"msg":"Hello world"}
 
+print('hello world')

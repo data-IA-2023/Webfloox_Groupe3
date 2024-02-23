bcda1f0e422e82c04cbebbb763c133b23afb103a
comewlecome <mohasoft@gmail.com>
2024-02-23 20:12:50 +0100

    recommandation knn

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLALchemy
-from dotenv import load_dotenv
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 
 
-load_dotenv('BDD_URL.env')
-BDD_URL = os.environ['BDD_URL']
 
-
-app= Flask(__name__,template_folder='template')
-app.config['SQLALchemy_DATABASE_URI'] = BDD_URL
-db= SQLALchemy(app)
+app = Flask(__name__, template_folder='template')
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    id=db.Column(db.Integer, primary_key=True )
-    content=db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    data_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
+    # Within the route function, you are already within the application context
     return render_template('index.html')
 
-if __name__=="__main__":
+# Ensure that any database operations are done within a request context
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+if __name__ == "__main__":
     app.run(debug=True)

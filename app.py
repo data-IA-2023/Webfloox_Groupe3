cbfabbeae458c4eb19df8b1a2dd06501caa05317
comewlecome <mohasoft@gmail.com>
2024-02-23 06:10:17 +0100

    flask simple

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLALchemy
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv('BDD_URL.env')
+BDD_URL = os.environ['BDD_URL']
+
 
 app= Flask(__name__,template_folder='template')
+app.config['SQLALchemy_DATABASE_URI'] = BDD_URL
+db= SQLALchemy(app)
+
+class Todo(db.Model):
+    id=db.Column(db.Integer, primary_key=True )
+    content=db.Column(db.String(200), nullable=False)
+    
 
 @app.route('/')
 def index():
--- main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI, Request
-app = FastAPI()
-@app.get("/")
-def get_root(request: Request):
-    return {'m':{ 'msg1' : "Hello, world"},'k':{ 'msg2' : "Hello, world"}}
